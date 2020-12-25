9ab6ac2695f86fd83ac3b1a5dee56f9da1569828
sejoon1011 <sejoon9895@gmail.com>
2020-12-25 23:45:34 +0900

    first commit

---
+++ Christmas.js
@@ -0,0 +1,9 @@
+window.onload = function (){
+const SNOW = new Snowflakes({
+    color : '#ffffff',
+    count : '24',
+    minOpacity: 1.0, // 최소 투명도 0: 투명 | 1: 불투명
+    maxOpacity: 1.0 // 최대 투명도
+
+})
+}
